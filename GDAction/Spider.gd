e10c2bd68b8df3a7ba1f4bc3a127b1a6ab97a99d
vivisuke <ntsuda@master.email.ne.jp>
2021-08-27 10:46:33 +0900

    0017 ◎ C 08/27 08/27 0.001│ToDo│クモ：空中にいるときは左右移動しないように

--- GDAction/Spider.gd
+++ GDAction/Spider.gd
@@ -1,24 +1,24 @@
 extends KinematicBody2D
 
 export (int) var walk_speed = 100
-export (int) var gravity = 1200
+export (int) var gravity = 800
 
-var velocity = Vector2(-walk_speed, 0)
+var velocity = Vector2(0, 1)
+var falling = true
 var walk_left = true
+
 
 func _ready():
 	pass # Replace with function body.
 
 
 func _physics_process(delta):
-	velocity.y += gravity
+	velocity.y += gravity*delta
 	velocity = move_and_slide(velocity)
-	#print("vel.x = ", velocity.x)
-	#print("get_slide_count() = ", get_slide_count())
-	#for i in get_slide_count():
-	#	var collision = get_slide_collision(i)
-	#	print("I collided with ", collision.collider.name)
-	if velocity.x == 0:
+	if falling && velocity.y == 0:
+		falling = false
+		velocity.x = -walk_speed if walk_left else walk_speed
+	if !falling && velocity.x == 0:
 		walk_left = !walk_left
 		velocity.x = -walk_speed if walk_left else walk_speed
 		$AnimatedSprite.flip_h = !walk_left
