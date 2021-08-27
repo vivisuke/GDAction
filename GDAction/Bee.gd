c52f8517192a443b83d281ebb476a4583e3ded9a
vivisuke <ntsuda@master.email.ne.jp>
2021-08-27 15:15:55 +0900

    0024 ◎ C 08/27 08/27 0.001│ToDo│（自動追尾する）蜂シーン追加

---
+++ GDAction/Bee.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var player
+var v = Vector2(0, 0)
+
+func _ready():
+	player = get_node("/root/MainScene/Player")
+	print(player)
+	$AnimatedSprite.play("fly")
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	if position.x - player.position.x < -10:
+		#scale.x = -1
+		$AnimatedSprite.flip_h = true
+		v.x = 100
+	elif position.x - player.position.x > 10:
+		#scale.x = 1
+		$AnimatedSprite.flip_h = false
+		v.x = -100
+	else:
+		return
+	move_and_collide(v*delta)
